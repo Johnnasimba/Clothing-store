9bf5fba53ebc928dff5f56e5879acc53f6103e0d
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-13 17:31:03 +0200

    Added a Homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HomePage from './homepage.component';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>E-commerce site </h1>
-    </div>
+      <HomePage/>
+    </div> 
   );
 }
 
