fb9522264835c9f9dbc6912e80677eb21c65761e
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-16 17:55:31 +0200

    Wrapped buttons in div container for advanced styling

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,9 +47,12 @@ class SignIn extends Component {
                         handleChange={this.handleChange}
                         label="password"
                         required />
-
-                    <CustomButton type="submit"  > Sign In</CustomButton>
-                    <CustomButton onClick ={signInWithGoogle}   isGoogleSignIn > Sign In with Google </CustomButton>
+                    
+                    <div className="buttons">
+                        <CustomButton type="submit"  > Sign In</CustomButton>
+                        <CustomButton onClick ={signInWithGoogle}   isGoogleSignIn > Sign In with Google </CustomButton>
+                    </div>
+                   
                 </form>
             </div>
           );
