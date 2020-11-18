7797897e4f076658b23b0e7db5d5e5dd31d7118a
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-18 15:23:55 +0200

    Created handleChange function

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -15,6 +15,29 @@ class SignUp extends Component {
             confirmPassword: ''
         }
     }
+    handleSubmit = async e => {
+        e.preventDefault();
+        const { displayName, email, password, confirmPassword } = this.state;
+        if (password !== confirmPassword) {
+            alert("passwords don't match");
+            return;
+        } 
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+            await createUserProfileDocument(user, { displayName });
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            });
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+
+
     render() { 
         const { displayName, email, password, confirmPassword } = this.state;
         return ( 
