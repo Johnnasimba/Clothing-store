ee3db7c2cdad640cca9c08296e461dfbb0d23123
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-19 13:23:34 +0200

    Conditionally render cartDropdown based on hidden value

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
-  const Header = ({currentUser}) => {
+  const Header = ({currentUser, hidden}) => {
       return ( 
           <div className="header">
             <Link className="logo-container" to="/" >
@@ -25,12 +25,16 @@ import './header.styles.scss';
                }
                <CartIcon/>
             </div>
-            <CartDropdown />
+            {
+               hidden ? null :  <CartDropdown />
+            }
+           
           </div>
        );
 }
-const mapStateToProps = state => ({
-     currentUser: state.user.currentUser
-  })
+const mapStateToProps = ({ user: {currentUser}, cart: {hidden}})=> ({
+   currentUser,
+   hidden
+})
    
   export default connect(mapStateToProps)(Header);
