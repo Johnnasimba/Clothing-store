e5b588ed1f862b56bc851f1ff470c1da37e373a3
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-23 15:07:32 +0200

    Connected currentUser and hidden selectors to the header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selector';
+
   const Header = ({currentUser, hidden}) => {
       return ( 
           <div className="header">
@@ -32,9 +36,9 @@ import './header.styles.scss';
           </div>
        );
 }
-const mapStateToProps = ({ user: {currentUser}, cart: {hidden}})=> ({
-   currentUser,
-   hidden
+const mapStateToProps = createStructuredSelector ({
+   currentUser: selectCurrentUser,
+   hidden: selectCartHidden
 })
    
   export default connect(mapStateToProps)(Header);
