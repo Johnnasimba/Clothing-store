12f67ded238758a249590ebcabe7d559bbe06f77
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-26 11:15:26 +0200

    Added redux-persist to make sure that client does not lose their carts whenever they refresh the page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'; 
-import store from './redux/store/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 
 import './index.css';
@@ -11,8 +12,10 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store} >
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+           <App />
+        </PersistGate>
     </BrowserRouter>,
   </React.StrictMode>,
   </Provider>,
