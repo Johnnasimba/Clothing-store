c2df62fe7066d699fcba59fa699a7f6378ab21db
JOHN NASIMBA <nasimba4john@gmail.com>
2021-01-27 23:56:34 +0200

    Creating redux files

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -15,7 +15,8 @@ class ShopPage extends Component {
   componentDidMount() {
     const collectionRef = firestore.collection('collections');
     collectionRef.onSnapshot(async snapshot => {
-      convertCollectionsSnapshotToMap(snapshot)
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      console.log(collectionsMap);
       
     })
     
