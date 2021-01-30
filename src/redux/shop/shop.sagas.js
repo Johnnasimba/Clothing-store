7c509383880d4b5539d5dce6a70aeced9c0bcff0
JOHN NASIMBA <nasimba4john@gmail.com>
2021-01-30 13:20:09 +0200

    Replacing redux-thunk with redux-saga for async code

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,10 +1,24 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+} from './shop.actions';
 
 import ShopActionType from './shop.types'
 
 export function* fetchCollectionsAsync() {
-    
-      yield  console.log('I am fired');
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+        
+        
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message))
+        
+    }
 }
 
 export function* fetchCollectionStart() {
